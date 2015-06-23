825eddac2807cde3f2a954c24d8249da94275c4f
Mike Land <mikepland@gmail.com>
2015-06-22 22:19:30 -0500

    adds shellScript dir and file

---
+++ shellScripts/cpScript.sh
@@ -0,0 +1,64 @@
+# cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0001.png ~/newDay/timeKillAppAssets/vault\ wheels/smallHandle0001.png
+# cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0001.png ~/newDay/timeKillAppAssets/vault\ wheels/largeHandle0001.png
+# mv ~/newDay/timeKillAppAssets/vault\ wheels/handle0001.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumHandle0001.png
+#
+cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0002.png ~/newDay/timeKillAppAssets/vault\ wheels/smallHandle0002.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0002.png ~/newDay/timeKillAppAssets/vault\ wheels/largeHandle0002.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/handle0002.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumHandle0002.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0003.png ~/newDay/timeKillAppAssets/vault\ wheels/smallHandle0003.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0003.png ~/newDay/timeKillAppAssets/vault\ wheels/largeHandle0003.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/handle0003.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumHandle0003.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0004.png ~/newDay/timeKillAppAssets/vault\ wheels/smallHandle0004.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0004.png ~/newDay/timeKillAppAssets/vault\ wheels/largeHandle0004.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/handle0004.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumHandle0004.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0005.png ~/newDay/timeKillAppAssets/vault\ wheels/smallHandle0005.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/handle0005.png ~/newDay/timeKillAppAssets/vault\ wheels/largeHandle0005.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/handle0005.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumHandle0005.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0001.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0001.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0001.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0001.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0001.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0001.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0002.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0002.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0002.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0002.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0002.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0002.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0003.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0003.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0003.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0003.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0003.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0003.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0004.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0004.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0004.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0004.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0004.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0004.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0005.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0005.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0005.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0005.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0005.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0005.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0006.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0006.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0006.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0006.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0006.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0006.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0007.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0007.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0007.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0007.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0007.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0007.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0008.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0008.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0008.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0008.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0008.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0008.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0009.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0009.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0009.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0009.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0009.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0009.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0010.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0010.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0010.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0010.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0010.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0010.png
+
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0011.png ~/newDay/timeKillAppAssets/vault\ wheels/smallWheels0011.png
+cp ~/newDay/timeKillAppAssets/vault\ wheels/wheels0011.png ~/newDay/timeKillAppAssets/vault\ wheels/largeWheels0011.png
+mv ~/newDay/timeKillAppAssets/vault\ wheels/wheels0011.png ~/newDay/timeKillAppAssets/vault\ wheels/mediumWheels0011.png
+
